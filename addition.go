b02c360eb6702bb7a25ef59ca8a5b14634934e3c
Tara HoleInIt <tara.a.k.86@gmail.com>
2026-08-11 12:28:34 -0400

    Change add to generic

--- addition.go
+++ addition.go
@@ -1,9 +1,17 @@
 // Package addition offers a complicated way of adding two numbers
 package addition
 
+import "golang.org/x/exp/constraints"
+
+// Number is the set of types that support addition via the + operator:
+// any integer or floating-point type.
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add adds two numbers and returns the result.
 // More information about [Addition] can be found on the mathisfun website.
 // [Addition]: https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TaraHoleInIt/addition
 
 go 1.26.5
+
+require golang.org/x/exp v0.0.0-20260811152304-ee035b5b010f // indirect
