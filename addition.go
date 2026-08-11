2ede92c2e894b4f896c336e74c812ed2b386878b
Tara HoleInIt <tara.a.k.86@gmail.com>
2026-08-11 12:20:12 -0400

    Add godoc comments

--- addition.go
+++ addition.go
@@ -1,5 +1,9 @@
+// Package addition offers a complicated way of adding two numbers
 package addition
 
+// Add adds two numbers and returns the result.
+// More information about [Addition] can be found on the mathisfun website.
+// [Addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
